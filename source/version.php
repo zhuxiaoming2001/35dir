06297f81fd7473f12cc33b6f2878613fe11f1b16
zhuxiaoming2001 <93664648+zhuxiaoming2001@users.noreply.github.com>
2025-02-21 22:04:43 +0800

    更新版本信息

--- source/version.php
+++ source/version.php
@@ -1,6 +1,6 @@
 <?php
 define('SYS_NAME', '优站分类目录管理系统');
-define('SYS_VERSION', 'YZDIR-v2.5');
-define('SYS_RELEASE', '20161224');
+define('SYS_VERSION', '35分类目录修订版-v2.5');
+define('SYS_RELEASE', '20250220');
 define('SYS_UPDATE', '【祥哥修订版】');
 ?>
